3fffc739f66f321805db77cef1d42a791bbf2ebf
Manasa <manasamaddipatla226@gmail.com>
2024-12-29 15:45:48 +0800

    Login Table In EmployeeServce

--- Springbootapp/src/main/java/com/org/host/sprbootpkg/model/LoginDAO.java
+++ Springbootapp/src/main/java/com/org/host/sprbootpkg/model/LoginDAO.java
@@ -1,8 +1,30 @@
 package com.org.host.sprbootpkg.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
+@Table
+@Entity
 public class LoginDAO {
-	
-	
+
+	private String username;
+
+	private String password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }
