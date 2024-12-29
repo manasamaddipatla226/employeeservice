8f7d93645ece78ab400e3793c51593a78a44e63d
Manasa <manasamaddipatla226@gmail.com>
2024-12-29 15:57:57 +0800

    Login Controller n EmployeeService

--- Springbootapp/src/main/java/com/org/host/sprbootpkg/controller/LoginController.java
+++ Springbootapp/src/main/java/com/org/host/sprbootpkg/controller/LoginController.java
@@ -1,9 +1,13 @@
 package com.org.host.sprbootpkg.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
-
+@Controller
+@RequestMapping
 public class LoginController {
 	
 	
